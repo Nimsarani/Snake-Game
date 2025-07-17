46114ab0bc11ff78353906b9fd2e77d43b5b291a
malsha <nimsarani@gmail.com>
2025-07-17 10:51:00 +0530

    Methods Create

---
+++ src/GamePanel.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    public void starGame(){
+
+    }
+    public void paintComponents(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+
+}
